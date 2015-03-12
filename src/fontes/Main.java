b8ebf215e6d9f2b57e2add7b8e36d6880b77185e
unknown <lucas.ferrari.ruh@gmail.com>
2015-03-11 21:44:12 -0300

    aula 11_03 - requisito 1

---
+++ src/fontes/Main.java
@@ -0,0 +1,12 @@
+package fontes;
+
+import model.Tabuleiro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tabuleiro tab = new Tabuleiro();
+		tab.iniciarTabuleiro();
+	}
+
+}
